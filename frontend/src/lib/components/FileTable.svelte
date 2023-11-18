e0f87bbd3e447180133ce6898fd704f4989e62f2
celenny <celennycs@gmail.com>
2023-11-18 16:26:41 -0300

    refatorando codigo

--- frontend/src/lib/components/FileTable.svelte
+++ frontend/src/lib/components/FileTable.svelte
@@ -4,11 +4,9 @@
   export let title: string;
   export let totalPages = 1;
 
-  const regex = new RegExp("\/.+\/")
+  const regex = new RegExp("/.+/");
 
-  const route = $page.url.pathname.match(regex)![0]
-  // let prevPageNumber = Number($page.url.pathname.split("/")[2]) - 1;
-  // let nextPageNumber = Number($page.url.pathname.split("/")[2]) + 1;
+  const route = $page.url.pathname.match(regex)![0];
 </script>
 
 <div class="w-full h-screen pt-9 dark:bg-gray-800 overflow-scroll">
@@ -17,7 +15,7 @@
       <h2 class="text-lg font-medium text-gray-800 dark:text-white">
         {title}
       </h2>
-      <slot name="button"/>
+      <slot name="button" />
     </div>
 
     <div class="flex flex-col mt-6">
@@ -29,9 +27,9 @@
             <table
               class="min-w-full divide-y divide-gray-200 dark:divide-gray-700"
             >
-              <slot name="header"/>
+              <slot name="header" />
 
-              <slot name="body"/>
+              <slot name="body" />
             </table>
           </div>
         </div>
@@ -39,60 +37,16 @@
     </div>
 
     <div class="flex items-center justify-between mt-6">
-      <!-- <a
-        href="{prevPageNumber.toString()}"
-        class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-5 h-5 rtl:-scale-x-100"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-          />
-        </svg>
-
-        <span> previous </span>
-      </a> -->
-
       <div class="items-center hidden md:flex gap-x-3">
         {#each { length: totalPages } as _, i}
           <a
-            href="{route}{i+1}"
+            href="{route}{i + 1}"
             class="px-2 py-1 text-sm text-green-500 rounded-md dark:hover:bg-gray-600 dark:text-green-300 hover:bg-gray-100"
-          >{i+1}
+            >{i + 1}
           </a>
         {/each}
       </div>
-
-      <!-- <a
-        href={nextPageNumber.toString()}
-        class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-      >
-        <span> Next </span>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-5 h-5 rtl:-scale-x-100"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-          />
-        </svg>
-      </a> -->
     </div>
   </section>
-  <slot name="footer"/>
+  <slot name="footer" />
 </div>
--- frontend/src/lib/utils/convertFormToJson.ts
+++ frontend/src/lib/utils/convertFormToJson.ts
@@ -4,7 +4,7 @@ export function convertFormToJson(data: FormData) {
 
   for (const [key, value] of data.entries()) {
     if (key.startsWith('columnName')) {
-      // Extract column names and associate with the corresponding column type
+      // Extraindo os nomes das colunas e associando ao tipo de coluna correspondente
       const columnNumber = key.replace('columnName', '');
       const columnName = value.toString();
       const columnTypeKey = `columnType${columnNumber}`;
